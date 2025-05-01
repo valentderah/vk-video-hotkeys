ba83672778843bebcb53b89860baaa571cfd2b76
Valiantsin Dzerakh <valentderah@gmail.com>
2025-05-01 13:30:59 +0300

    add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path')
+
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
+const miniExtractPlugin = require('mini-css-extract-plugin')
 
 const config = {
     entry: {
         background: './src/background.js',
-        render: './src/render.js'
+        render: './src/render.js',
+        style: './src/style.css'
     },
     output: {
         filename: '[name].js',
@@ -22,8 +25,23 @@ const config = {
                     from: 'public'
                 }
             ]
-        })
+        }),
+        new miniExtractPlugin()
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    miniExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+
+    mode: "production",
+
     optimization: {
         minimize: true
     }
